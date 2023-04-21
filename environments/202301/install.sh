f2b6b86be618f28547889d7846129e22c30ac728
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2023-04-21 11:40:56 +0200

    Remove conda-lock files, wait for better update options

--- environments/202301/install.sh
+++
@@ -1,2 +0,0 @@
-mamba env update -n 202301 -f ./conda-linux-64.lock.yml
-# conda-lock install --name 202301 conda-lock.yml
--- environments/202301/lock.sh
+++
@@ -1,2 +0,0 @@
-conda-lock -f base.yml -f custom.yml --no-mamba --lockfile conda-lock.yml -p linux-64
-conda-lock render -e base -e custom -p linux-64 -k env -k explicit ./conda-lock.yml
