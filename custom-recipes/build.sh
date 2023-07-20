9445beb5b40387d7873fe3db01284ce6993b23a5
Robert Rosca <robert.rosca@xfel.eu>
2023-07-20 15:24:43 +0200

    Pass channels as args instead of setting config

--- custom-recipes/build.sh
+++ custom-recipes/build.sh
@@ -3,10 +3,16 @@
 set -euo pipefail
 shopt -s inherit_errexit
 
-conda config --add channels conda-forge
-conda config --add channels ./conda-bld
 conda index ./conda-bld
 
 export CONDA_BLD_PATH="$PWD/conda-bld"
 
-conda mambabuild --skip-existing --python 3.9 --numpy 1.23 --no-anaconda-upload --use-local ./recipes
+conda mambabuild \
+  --skip-existing \
+  --python 3.9 \
+  --numpy 1.23 \
+  --no-anaconda-upload \
+  -c nodefaults -c conda-forge -c $CONDA_BLD_PATH \
+  ./recipes
+
+conda index ./conda-bld
