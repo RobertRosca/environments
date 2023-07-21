752fd5a5642d4fb891c24b6b84926d6735904163
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2023-07-21 09:23:58 +0200

    Use boa recipe format instead of meta.yaml (#8)

--- custom-recipes/build.sh
+++ custom-recipes/build.sh
@@ -3,16 +3,34 @@
 set -euo pipefail
 shopt -s inherit_errexit
 
+
+if [ $# -eq 0 ]; then
+  p="./recipes"
+elif [ $# -eq 1 ]; then
+  p="./recipes/$1"
+else
+  echo "Usage: $0 [package-name]"
+  exit 1
+fi
+
+cleanup_tmp_shm() {
+    if [ -d "$tmp_shm" ]; then
+        echo "Cleaning up temporary directory: $tmp_shm"
+        rm -rf "$tmp_shm"
+    fi
+}
+
+trap cleanup_tmp_shm EXIT
+
+tmp_shm=$(mktemp -d -p /dev/shm boa-XXXX)
+
 conda index ./conda-bld
 
-export CONDA_BLD_PATH="$PWD/conda-bld"
-
-conda mambabuild \
+boa build \
   --skip-existing \
-  --python 3.9 \
-  --numpy 1.23 \
-  --no-anaconda-upload \
-  -c nodefaults -c conda-forge -c $CONDA_BLD_PATH \
-  ./recipes
+  --croot $tmp_shm \
+  --output-folder ./conda-bld \
+  --target-platform linux-64 \
+  $p
 
 conda index ./conda-bld
